c62317ff7a47a4dcc95b4c42e22774db76548626
JustRogueChicken <164870328+JustRogueChicken@users.noreply.github.com>
2024-05-16 00:12:59 +0700

    Add files via upload

---
+++ Lab3_1/Bai3_1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using Bai3_1;
+using System.Windows.Forms;
+using System;
+
+namespace Bai3_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_server_Click(object sender, EventArgs e)
+        {
+            Server server = new Server();
+            server.Show();
+        }
+
+        private void btn_client_Click(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            client.Show();
+        }
+    }
+}
